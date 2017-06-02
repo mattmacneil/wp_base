fa4c8cfe61f0ff885a7e92cd31610adb9c3d9539
Matt MacNeil <matt@mattmacneil.com>
2017-06-02 18:27:12 -0400

    Created single page template.

---
+++ wp-content/themes/mattmacneil/single.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template Name: Default Page
+ * Description: Default Page for Base Custom Theme.
+ */
+
+	get_header();
+
+	 // get_template_part( 'template-parts/hero' );
+
+?>
+
+	single.php
+
+<?php get_footer(); ?>
