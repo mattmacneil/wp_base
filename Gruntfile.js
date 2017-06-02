5fd919098a4cd4ad2722e620850e20da5e28cbeb
Matt MacNeil <matt@mattmacneil.com>
2017-06-02 16:18:39 -0400

    Configured grunt files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'wp-content/themes/mattmacneil/assets/css/sass',
+          cwd: 'wp-content/themes/mattmacneil/assets/css',
           src: ['**/*.scss'],
           dest: 'wp-content/themes/mattmacneil/',
           ext: '.css'
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
 
         map: {
             inline: false,
-            annotation: 'wp-content/themes/mattmacneil/assets/css/sass/maps'
+            annotation: 'wp-content/themes/mattmacneil/assets/css/maps'
         },
 
         processors: [
